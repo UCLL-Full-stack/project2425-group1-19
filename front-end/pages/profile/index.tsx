3e4ff18067ffffe40448608e14583d0343e7af66
Abdulrahman-Al-Hasbani-UCLL <abdulrahman.alhasbani@student.ucll.be>
2024-11-03 15:08:41 +0100

    Needed pages added as a basic

---
+++ front-end/pages/profile/index.tsx
@@ -0,0 +1,23 @@
+import Header from "@/components/header";
+import Head from "next/head";
+import React from "react";
+
+const monthlySpendings: React.FC = () => {
+    return (
+        <>
+            <Head>
+                <title>Profile</title>
+            </Head>
+            <header>
+                <nav>
+                    <Header />
+                </nav>
+            </header>
+            <main>
+                <h1>Under development</h1>
+            </main>
+        </>
+    )
+};
+
+export default monthlySpendings;
